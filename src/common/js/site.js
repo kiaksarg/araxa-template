5a644c8fbb0e6e167b29f252a982745beb6a9646
kiaksarg <kiaksarg@gmail.com>
2016-12-05 05:15:15 +0330

    Add nav

--- src/common/js/site.js
+++ src/common/js/site.js
@@ -4,3 +4,7 @@ $(document).ready(function() {
         interval: false,
     });
 });
+// // .nav>li:hover>a:after
+// $(".nav>li:hover>a").after("<i class='fa fa-chevron-up' aria-hidden='true'></i>");
+
+// $(".nav>li>a").after("<i class='fa fa-chevron-down' aria-hidden='true'></i>");
