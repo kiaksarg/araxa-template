825a0c8aa7e8ef7bd909d1671761ec47ef026ede
kiaksarg <kiaksarg@gmail.com>
2017-02-01 06:16:42 +0330

    Avoid dropdown menu close on click inside

--- src/common/js/site.js
+++ src/common/js/site.js
@@ -8,3 +8,8 @@ $(document).ready(function() {
 // $(".nav>li:hover>a").after("<i class='fa fa-chevron-up' aria-hidden='true'></i>");
 
 // $(".nav>li>a").after("<i class='fa fa-chevron-down' aria-hidden='true'></i>");
+
+
+$(document).on('click', '.submenu-offcanvas', function(e) {
+    e.stopPropagation();
+});
